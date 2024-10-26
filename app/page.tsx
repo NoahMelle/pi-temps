b7b7fc3dd53bea9911132c95438f05457112d7e6
= <noahmkamphuisen@gmail.com>
2024-10-26 17:41:38 +0200

    hotfix: not dynamic on prod

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,27 @@
 import TempCircle from "@/components/TempCircle";
 import fs from "fs/promises";
 
+export const dynamic = "force-dynamic";
+
 export default async function Home() {
-  const runningOnPi = process.env.PI_ENV === "pi"
+    const runningOnPi = process.env.PI_ENV === "pi";
+    let temp = 50;
 
-  const temp = runningOnPi ? ((Number((await fs.readFile("/sys/class/thermal/thermal_zone0/temp", "utf-8"))) / 1000) || 0) : 50
+    if (runningOnPi) {
+        try {
+            const tempData = await fs.readFile(
+                "/sys/class/thermal/thermal_zone0/temp",
+                "utf-8"
+            );
+            temp = Number(tempData) / 1000 || 0;
+        } catch (error) {
+            console.error("Failed to read temperature:", error);
+        }
+    }
 
-  return (
-    <div className="h-screen w-full flex items-center justify-center">
-      <TempCircle targetTemp={temp}/>
-    </div>
-  );
+    return (
+        <div className="h-screen w-full flex items-center justify-center">
+            <TempCircle targetTemp={temp} />
+        </div>
+    );
 }
