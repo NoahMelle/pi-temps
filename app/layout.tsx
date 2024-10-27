eec574ee9a534b29c9ceb57bac96e51a9a050354
= <noahmkamphuisen@gmail.com>
2024-10-27 18:15:28 +0100

    added theme switching

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,27 +1,27 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
+import ThemeToggle from "@/components/ThemeToggle";
 
 const inter = localFont({
-  src: "./fonts/Inter.ttf",
+    src: "./fonts/Inter.ttf",
 });
 
 export const metadata: Metadata = {
-  title: "Noah's Temperatures",
+    title: "Noah's Temperatures",
 };
 
 export default function RootLayout({
-  children,
+    children,
 }: Readonly<{
-  children: React.ReactNode;
+    children: React.ReactNode;
 }>) {
-  return (
-    <html lang="en">
-      <body
-        className={inter.className}
-      >
-        {children}
-      </body>
-    </html>
-  );
+    return (
+        <html lang="en">
+            <body className={inter.className}>
+                <ThemeToggle />
+                {children}
+            </body>
+        </html>
+    );
 }
